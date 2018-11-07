423c1eaa6a7c23597391143746df5fa45aff3866
iamsoroush <soroush.moazed@gmail.com>
2018-11-07 14:53:45 +0330

    code made cleaner

--- pycw.py
+++ pycw.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
-from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
+from sklearn.metrics.pairwise import pairwise_distances
 
 
 class ChineseWhispers(BaseEstimator, ClusterMixin):
@@ -11,13 +11,18 @@ class ChineseWhispers(BaseEstimator, ClusterMixin):
 
     """
 
-    def __init__(self, n_iterations=10, metric='euclidean'):
+    def __init__(self, n_iteration=10, metric='euclidean'):
         """Init an estimator object.
 
         Parameters
         ----------
         n_iterations: int
             Number of iterations.
+
+        metric: str
+            String indicating metric to use in calculating distances between samples.
+            For available metrics refer to:
+             http://scikit-learn.org/0.18/modules/generated/sklearn.metrics.pairwise.pairwise_distances.html
 
         Attributes
         ----------
@@ -29,7 +34,7 @@ class ChineseWhispers(BaseEstimator, ClusterMixin):
             Similarity measure is: 1/euclidean_distance.
 
         """
-        self.n_iterations = n_iterations
+        self.n_iteration = n_iteration
         self.metric = metric
         self.labels_ = None
         self.adjacency_mat_ = None
@@ -46,45 +51,64 @@ class ChineseWhispers(BaseEstimator, ClusterMixin):
 
         """
 
-        available_metrics = ['euclidean', 'cosine']
-        assert isinstance(self.n_iterations, int), "parameter n_iterations must be of type int"
+        assert isinstance(self.n_iteration, int), "parameter n_iterations must be of type int"
         assert isinstance(X, np.ndarray), "X must be an instance of np.ndarray"
         assert X.ndim == 2, "X must be of ndim=2"
-        assert self.metric in available_metrics, "Invalid metric."
+
+        graph_clustering = ChineseWhispersClustering(n_iteration=self.n_iteration)
+        adjacency_mat = self._generate_adjacency_mat(X)
+        labels = graph_clustering.fit_predict(adjacency_mat)
+        self.adjacency_mat_ = adjacency_mat
+        self.labels_ = labels
+        return labels
+
+    def _generate_adjacency_mat(self, X):
         n_samples = X.shape[0]
-        if self.metric == 'euclidean':
-            adjacency_mat = (1 / (euclidean_distances(X, X) + np.identity(n_samples, dtype=X.dtype))) *\
-                            (np.ones((n_samples, n_samples), dtype=X.dtype) -
-                             np.identity(n_samples, dtype=X.dtype))
-        else:
-            adjacency_mat = cosine_similarity(X, X) *\
-                            (np.ones((n_samples, n_samples), dtype=X.dtype) -
-                             np.identity(n_samples, dtype=X.dtype))
-        indices = np.arange(n_samples)
-        labels_mat = np.arange(n_samples)
-        for _ in range(self.n_iterations):
+        distances_mat = pairwise_distances(X, metric=self.metric)
+        adjacency_mat = (1 / (distances_mat + np.identity(n_samples, dtype=X.dtype))) *\
+                        (np.ones((n_samples, n_samples), dtype=X.dtype) -
+                         np.identity(n_samples, dtype=X.dtype))
+        return adjacency_mat
+
+    def score(self, X, y):
+        pass
+
+
+class ChineseWhispersClustering:
+    """ChineseWhispers algorithm.
+    This class got adjancency matrix of a graph as input
+    """
+
+    def __init__(self, n_iteration=5):
+        self.n_iteration = n_iteration
+        self.adjacency_mat_ = None
+        self.labels_ = None
+
+    def fit_predict(self, adjacency_mat):
+        """Fits and returns labels for samples"""
+
+        n_nodes = adjacency_mat.shape[0]
+        indices = np.arange(n_nodes)
+        labels_mat = np.arange(n_nodes)
+        for _ in range(self.n_iteration):
             np.random.shuffle(indices)
             for ind in indices:
                 weights = adjacency_mat[ind]
-                unique_labels = list(set(labels_mat))
-                label_weights = np.zeros(len(unique_labels))
-                for i, label in enumerate(unique_labels):
-                    args = np.where(labels_mat == label)[0]
-                    label_weights[i] = np.sum(weights[args])
-                labels_mat[ind] = unique_labels[np.argmax(label_weights)]
+                winner_label = self._find_winner_label(weights, labels_mat)
+                labels_mat[ind] = winner_label
         self.adjacency_mat_ = adjacency_mat
         self.labels_ = labels_mat
         return labels_mat
 
     @staticmethod
-    def maxrow(row):
-        """Returns a sparse vector of same size as input vector,
-        containing zeros except for maximum element, which is 1."""
-
-        argmax = np.argmax(row)
-        out = np.zeros(row.shape, dtype=np.int8)
-        out[argmax] = 1
-        return out
-
-    def score(self, X, y):
-        pass
+    def _find_winner_label(node_weights, labels_mat):
+        adjacent_nodes_indices = np.where(node_weights > 0)[0]
+        adjacent_nodes_labels = labels_mat[adjacent_nodes_indices]
+        unique_labels = np.unique(adjacent_nodes_labels)
+        label_weights = np.zeros(len(unique_labels))
+        for ind, label in enumerate(unique_labels):
+            indices = np.where(adjacent_nodes_labels == label)
+            weight = np.sum(node_weights[adjacent_nodes_indices[indices]])
+            label_weights[ind] = weight
+        winner_label = unique_labels[np.argmax(label_weights)]
+        return winner_label
